6a44e71d1004cce47f6b76cd8da6a4397069a014
nowak1213 <nowak2809@gmail.com>
2020-02-04 15:39:25 +0100

    Update PaymentsService.js
    Remove CAPTURED flag from response statuses while querying

--- service/PaymentsService.js
+++ service/PaymentsService.js
@@ -129,7 +129,7 @@ exports.prepareReservationPayment = function(request,authorization,xMobilePayCli
  **/
 exports.queryPayment = function(paymentId,authorization,xMobilePayClientId,xMobilePayClientSystemName,xMobilePayClientSystemVersion) {
   return new Promise(function(resolve, reject) {
-    var statuses = ["Initiated", "IssuedToUser", "Reserved", "Captured"];
+    var statuses = ["Initiated", "IssuedToUser", "Reserved"];
 
     if (payments.has(paymentId)) {
       var count = payments.get(paymentId);
